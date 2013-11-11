d14fb1675378af5d5ec227df7d10144468d934b4
Paul Schyska <pschyska@gmail.com>
2013-11-11 20:11:56 +0100

    Shows project description teaser and links to mobile donation form

--- app/scripts/controllers/project_details.js
+++ app/scripts/controllers/project_details.js
@@ -5,4 +5,19 @@ angular.module('bettertweetApp')
     Project.get({id: $routeParams.id}, function(project) {
       $scope.project = project;
     });
+
+    $scope.getPlatformLink = function() {
+      for(var i = 0; i < $scope.project.links.length; i++) {
+        if($scope.project.links[i]["rel"] == "platform") {
+          return $scope.project.links[i]["href"];
+        }
+      }
+    }
+  }).filter('truncate', function() {
+    return function(value, length) {
+      length = length || 15;
+      var div = document.createElement("div");
+      div.innerHTML = value;
+      return div.innerText.substring(0, length - 3)+" &hellip;"
+    }
   });
